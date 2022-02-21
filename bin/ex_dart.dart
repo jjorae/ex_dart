2b3980693d643658f61523197a254ffcae964628
jjorae <raehyeok.cho@gmail.com>
2022-02-22 00:10:56 +0900

    Initial commit

---
+++ bin/ex_dart.dart
@@ -0,0 +1,5 @@
+import 'package:ex_dart/ex_dart.dart' as ex_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ex_dart.calculate()}!');
+}
---
+++ test/ex_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:ex_dart/ex_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
